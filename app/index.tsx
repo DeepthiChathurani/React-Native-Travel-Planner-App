b91c050f80eddbf2e12bad929a0785c5c2019d9b
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2024-07-30 14:51:41 +0530

    Login Page Add

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import { Text, View } from "react-native";
+import Login from "../components/Login"
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        
+
       }}
     >
-      <Text>SUBSCRIBE TO TUBEGURUJI !</Text>
+        <Login/>
     </View>
   )
 }
